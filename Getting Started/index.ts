9cc17dab4ec05fe8175f04663aff8a27947e40a0
Wesley Flôres <wesleyfloresterres@gmail.com>
2021-01-05 22:33:04 -0300

    index.ts

--- Getting Started/index.ts
+++ Getting Started/index.ts
@@ -2,12 +2,12 @@ window.onload = () => {
     const ButtonSubmit = document.querySelector('button[type=submit]')! as HTMLButtonElement;
 
     ButtonSubmit.addEventListener('click', (event) => {
-        const HTMLInputEmail = document.querySelector("#exampleInputEmail1")! as HTMLInputElement;
+        const HTMLInputEmail = document.querySelector("#exampleInputEmail")! as HTMLInputElement;
         const HTMLInputPassword = document.querySelector("#exampleInputPassword")! as HTMLInputElement;
 
         event.preventDefault();
 
-        const MessageOutput: String = `exampleInputEmail1: ${HTMLInputEmail.value} / exampleInputPassword: ${HTMLInputPassword.value}`;
+        const MessageOutput: String = `exampleInputEmail: ${HTMLInputEmail.value} / exampleInputPassword: ${HTMLInputPassword.value}`;
 
         console.log(MessageOutput);
     });
